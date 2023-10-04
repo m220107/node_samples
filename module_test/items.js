cf0844b9d629c8181893bba938685aed29f7956b
m220107 <220107ie@yse-c-net>
2023-10-04 10:25:32 +0900

    モジュールの読み込み

--- module_test/items.js
+++ module_test/items.js
@@ -1,3 +1,4 @@
+// モジュール化する
 exports.items = [  //ほかのJSファイルからitemsの変数が使えるようにする（ブラウザでは使えない）
     'Apple',
     'Orange',
---
+++ module_test/main.js
@@ -0,0 +1,7 @@
+// モジュールの読み込み
+const itemModule = require('./items')
+const controlModule = require('./control')
+
+
+var items = itemModule.items
+controlModule.show(items)
